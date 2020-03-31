3f565a30d80ac16181a01a475690880cce5538c5
Franck Théolade <frtheola@microsoft.com>
2020-03-31 19:46:42 +0100

    Added about functionality

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,10 +28,17 @@ namespace DemoLabApp.Controllers
             return View();
         }
 
+        
+        public IActionResult About()
+        {
+            return View();   
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return 
+            View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
